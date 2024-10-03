46a23523f28a5eea679ba9f079411fa3f26ae484
RedC0mrade <e2e4e2e4@mail.ru>
2024-10-03 20:42:07 +0200

    No more database create

--- app/main.py
+++ app/main.py
@@ -1,21 +1,11 @@
 import uvicorn
 
-from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
-from db_core.base import Base
-from db_core.engine import db_helper
-from users.user_model_db import UserAlchemyModel
 from users import views
 
 
-# @asynccontextmanager
-# async def lifespan(app: FastAPI):
-#     async with db_helper.engine.begin() as conn:
-#         await conn.run_sync(Base.metadata.create_all)
-#     yield
-
-# app = FastAPI(lifespan=lifespan)
 app =FastAPI()
+
 if __name__ == '__main__':
     uvicorn.run("main:app", reload=True)
